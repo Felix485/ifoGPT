5079b4cd687b6a4e8363f62635f5ebb3a2738a03
JuliaBaarck <52248396+JuliaBaarck@users.noreply.github.com>
2023-04-28 21:23:07 +0200

    frontend add logo

--- frontend.py
+++ frontend.py
@@ -3,11 +3,25 @@
 import streamlit as st
 def main():
     st.title("ifoGPT")
+    st.image("ifo_logo.png", width = 200)
+    st.markdown("""
+    <style>
+        body {
+            background-color: #0D4080;
+        }
+    </style>
+    """, unsafe_allow_html=True)
     st.subheader("A press release generator")
     user_input = st.text_input("Enter some text:")
     if st.button("Submit"):
         st.header("Suggested press release:")
         st.write(f"{user_input}")
+
+    prompt = user_input
+    result = myfunction(prompt)
+
+
+
 if __name__ == "__main__":
     main()
 
