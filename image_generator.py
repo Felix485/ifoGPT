970ba84de4016aab60e11c79c6c81b6443cdef1c
JuliaBaarck <52248396+JuliaBaarck@users.noreply.github.com>
2023-04-29 17:38:16 +0200

    image_generator macht jetzt schöne Bilder (ifo blau, cv foto)

--- image_generator.py
+++ image_generator.py
@@ -2,23 +2,28 @@ from PIL import Image, ImageDraw, ImageFont
 import textwrap
 
 
-def create_image_with_text(logo_path, text, output_path, font_path=None, max_width=None, input_path=None, line_spacing=5):
+def create_image_with_text(logo_path, text, output_path, font_path=None, max_width=None, input_path=None, line_spacing=30):
     # Load the logo image
     logo = Image.open(logo_path)
 
     # Create an empty image with white background
-    img_w, img_h = 1000, 1000
-    background = Image.new('RGBA', (img_w, img_h), (255, 255, 255, 255))
+    img_w, img_h = 4000, 2000
+    background = Image.new('RGBA', (img_w, img_h), (13,64,128, 255))
 
     # Paste the logo onto the background
     background.paste(logo, (20, 870))
     im = Image.open(input_path)
-    background.paste(im, (20, 20))
+    im = im.resize((2000,2000))
+    # Define the cropping area (left, upper, right, lower)
+    crop_area = (300, 0, 1700, 2000)
+    # Crop the image
+    cropped_image = im.crop(crop_area)
+    background.paste(cropped_image, (0,0))
 
     # Create a draw object and font object
     draw = ImageDraw.Draw(background)
     if font_path:
-        font = ImageFont.truetype(font_path, 60)
+        font = ImageFont.truetype(font_path, 120)
     else:
         font = ImageFont.load_default()
 
@@ -30,22 +35,22 @@ def create_image_with_text(logo_path, text, output_path, font_path=None, max_wid
     # Calculate the size and position of the text
     text_bbox = draw.multiline_textbbox((0, 0), wrapped_text, font=font, spacing=line_spacing, align='center')
     text_w, text_h = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
-    text_x = (img_w - text_w) // 2
-    text_y = img_h - (text_h // 2) - 200
+    text_x = (img_w - text_w + 1400) // 2
+    text_y = (img_h//2) - (text_h // 2) 
 
     # Draw the text onto the image
-    draw.multiline_text((text_x, text_y), wrapped_text, font=font, fill=(0, 0, 0, 255), spacing=line_spacing, align='center')
+    draw.multiline_text((text_x, text_y), wrapped_text, font=font, fill=(255, 255, 255, 255), spacing=line_spacing, align='center')
 
     # Save the image
     background.save(output_path)
 
 
 # Example usage:
-input_path = "downloaded_images/graphic_images/20230428-ifo-pm-bip-lang-0423-de.png"
+input_path = "downloaded_images\cv_images\cvfoto-schultz.jpg"
 logo_path = "ifo_logo.png"
 text = "Dies ist KEIN sehr langer Text über Wirtschaftsnachrichten. Er ist bestimmt sehr wichtig."
 output_path = "output.png"
 font_path = "WorkSans.ttf"  # Path to your desired font (optional)
-max_width = 25  # Maximum number of characters per line (optional)
+max_width = 35  # Maximum number of characters per line (optional)
 create_image_with_text(logo_path, text, output_path, font_path, max_width, input_path)
 
