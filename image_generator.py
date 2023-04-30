726367d00b0db7b77033da66e5b6539a28c5bfb8
Justus <Justus.Beck@campus.lmu.de>
2023-04-30 10:25:39 +0200

    made some code cleaner

--- image_generator.py
+++ image_generator.py
@@ -3,33 +3,33 @@ import textwrap
 import streamlit as st
 
 
-def create_image_with_text(text, logo_path="layer-1.png",  output_path="output.png", font_path="WorkSans.ttf", max_width=31, input_path="downloaded_images/cv_images/cvfoto-schultz.jpg", line_spacing=30):
+def create_image_with_text(text, logo_path="layer-1.png", output_path="output.png", font_path="WorkSans.ttf",
+                           max_width=31,
+                           input_path="downloaded_images/cv_images/cvfoto-schultz.jpg", line_spacing=30):
     # Load the logo image
-    logo = Image.open(logo_path)
+    logo_image = Image.open(logo_path)
 
-    # Create an empty image with white background
-    img_w, img_h = 4000, 2000
-    background = Image.new('RGBA', (img_w, img_h), (13,64,128, 255))
+    # Create an empty image with a specific background color
+    image_width, image_height = 4000, 2000
+    background_image = Image.new('RGBA', (image_width, image_height), (13, 64, 128, 255))
 
-    # Paste the logo onto the background
-    im = Image.open(input_path)
-    im = im.resize((2000,2000))
+    # Paste the input image onto the background
+    input_image = Image.open(input_path)
+    input_image = input_image.resize((2000, 2000))
+
     # Define the cropping area (left, upper, right, lower)
     crop_area = (300, 0, 1700, 2000)
-    # Crop the image
-    cropped_image = im.crop(crop_area)
-    #background.paste(logo, (0, 0))
+    # Crop the input image
+    cropped_image = input_image.crop(crop_area)
 
-    background.paste(im, (2000,0))
-    background.paste(logo, (0, 0), logo)
-
-
+    background_image.paste(input_image, (2000, 0))
+    background_image.paste(logo_image, (0, 0), logo_image)
 
     # Create a draw object and font object
-    draw = ImageDraw.Draw(background)
-    fill_color = (255, 255, 255)
+    draw = ImageDraw.Draw(background_image)
+    text_fill_color = (255, 255, 255)
 
-
+    # Load the font
     if font_path:
         font = ImageFont.truetype(font_path, 100)
     else:
@@ -37,7 +37,8 @@ def create_image_with_text(text, logo_path="layer-1.png",  output_path="output.p
 
     # Wrap the text into multiple lines if it's too long
     if max_width is None:
-        max_width = img_w
+        max_width = image_width
+
     try:
         wrapped_text = textwrap.fill(text, width=max_width, break_long_words=True, break_on_hyphens=True)
     except AttributeError as e:
@@ -46,23 +47,13 @@ def create_image_with_text(text, logo_path="layer-1.png",  output_path="output.p
 
     # Calculate the size and position of the text
     text_bbox = draw.multiline_textbbox((0, 0), wrapped_text, font=font, spacing=line_spacing, align='center')
-    text_w, text_h = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
-    text_x = (img_w - text_w - 2000) // 2
-    text_y = (img_h//2) - (text_h // 2) 
+    text_width, text_height = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
+    text_x = (image_width - text_width - 2000) // 2
+    text_y = (image_height // 2) - (text_height // 2)
 
     # Draw the text onto the image
-    draw.multiline_text((text_x, text_y), wrapped_text, font=font, fill=(255, 255, 255, 255), spacing=line_spacing, align='center')
+    draw.multiline_text((text_x, text_y), wrapped_text, font=font, fill=(255, 255, 255, 255),
+                        spacing=line_spacing, align='center')
 
     # Save the image
-    background.save(output_path)
-
-
-# Example usage:
-#input_path = "downloaded_images\cv_images\cvfoto-schultz.jpg"
-#logo_path = "ifo_logo.png"
-#text = "Dies ist KEIN sehr langer Text über Wirtschaftsnachrichten. Er ist bestimmt sehr wichtig."
-#output_path = "downloaded_images/output_image/output.png"
-#font_path = "WorkSans.ttf"  # Path to your desired font (optional)
-#max_width = 35  # Maximum number of characters per line (optional)
-#create_image_with_text(logo_path, text, output_path, font_path, max_width, input_path)
-
+    background_image.save(output_path)
--- scraping.py
+++ scraping.py
@@ -4,9 +4,9 @@ import os
 import re
 
 
-def download_image(url, save_path):
+def download_image(image_url, save_path):
     try:
-        response = requests.get(url, stream=True)
+        response = requests.get(image_url, stream=True)
         response.raise_for_status()
 
         with open(save_path, "wb") as file:
@@ -14,14 +14,10 @@ def download_image(url, save_path):
                 file.write(chunk)
         print(f"Image saved to {save_path}")
     except Exception as e:
-        print(f"Error downloading image from {url}: {e}")
+        print(f"Error downloading image from {image_url}: {e}")
 
 
-#url = 'https://www.ifo.de/pressemitteilung/2023-04-28/konsum-und-industrie-senden-gegensaetzliche-impulse-fuer-deutsche'
-
-
-
-def scrapeurl(url):
+def scrape_url(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -29,16 +25,12 @@ def scrapeurl(url):
     img_urls = [img.get('data-src') for img in img_tags]
     img_urls = [url for url in img_urls if url is not None]
 
-
     print(img_urls)
-
 
     save_directory = "downloaded_images"
     cv_directory = "cv_images"
     graphic_directory = "graphic_images"
     output_directory = "output_image"
-
-
 
     if not os.path.exists(save_directory):
         os.makedirs(save_directory)
@@ -49,35 +41,22 @@ def scrapeurl(url):
     if not os.path.exists(os.path.join(save_directory, output_directory)):
         os.makedirs(os.path.join(save_directory, output_directory))
 
-    directory = os.path.join(save_directory, cv_directory)
+    # Clear old files in the cv_images and graphic_images directories
+    for dir_name in [cv_directory, graphic_directory]:
+        directory = os.path.join(save_directory, dir_name)
 
-    # Get a list of all files in the directory
-    files = os.listdir(directory)
+        # Get a list of all files in the directory
+        files = os.listdir(directory)
 
-    # Loop through the files and delete each one
-    for file in files:
-        # Construct the full file path
-        file_path = os.path.join(directory, file)
+        # Loop through the files and delete each one
+        for file in files:
+            # Construct the full file path
+            file_path = os.path.join(directory, file)
 
-        # Check if the file is a file (not a subdirectory)
-        if os.path.isfile(file_path):
-            # Delete the file
-            os.remove(file_path)
-
-    directory = os.path.join(save_directory, graphic_directory)
-
-    # Get a list of all files in the directory
-    files = os.listdir(directory)
-
-    # Loop through the files and delete each one
-    for file in files:
-        # Construct the full file path
-        file_path = os.path.join(directory, file)
-
-        # Check if the file is a file (not a subdirectory)
-        if os.path.isfile(file_path):
-            # Delete the file
-            os.remove(file_path)
+            # Check if the file is a file (not a subdirectory)
+            if os.path.isfile(file_path):
+                # Delete the file
+                os.remove(file_path)
 
     # Download the images
     for url in img_urls:
@@ -89,8 +68,7 @@ def scrapeurl(url):
             save_path = os.path.join(save_directory, graphic_directory, filename)
         download_image(url, save_path)
 
-
-    # Text
+    # Scrape text
     title = soup.find('meta', attrs={'name': 'twitter:title'})['content']
     text_list = soup.find_all('p')
     text_string = " ".join([t.get_text() for t in text_list])
